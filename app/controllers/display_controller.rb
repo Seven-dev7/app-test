5ed75ea12a18715ddf24fb2e710ae3f9c5936bcd
Se7en Oussama <seven.tdr7@gmail.com>
2020-10-11 11:04:56 +0200

    add show view for all embassy data in table

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,8 +1,10 @@
 class DisplayController < ApplicationController
   def index
     @countries = Country.all
+    @embassies = Embassy.all
   end
 
   def show
+    @embassies = Embassy.all
   end
 end
