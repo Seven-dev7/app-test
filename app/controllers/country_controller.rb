afc5a2c16b642cd549c36819641812ba52451bca
Seven-dev7 <54798474+Seven-dev7@users.noreply.github.com>
2020-10-12 15:17:10 +0200

    Add gmap on show country (#3)
    * first commit for pr
    
    * add all thing in view and controller embassy to have the map with lati and longi
    
    Co-authored-by: Se7en Oussama <seven.tdr7@gmail.com>

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -8,5 +8,9 @@ class CountryController < ApplicationController
     foreigner_data = @country.foreigners
     @foreigner_year = foreigner_data.map{ |f| f.year}
     @embassies = @country.embassies
+    @hash = Gmaps4rails.build_markers(@embassies) do |embassy, marker|
+      marker.lat embassy.latitude
+      marker.lng embassy.longitude
+    end
   end
 end
