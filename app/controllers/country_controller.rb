606e7f478bec099e0d41b1d54d1ea7a24d0a7cd2
Se7en Oussama <seven.tdr7@gmail.com>
2020-10-12 14:56:53 +0200

    card show countries, add partials inside

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -5,7 +5,8 @@ class CountryController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
-    @foreigner_data = @country.foreigners
+    foreigner_data = @country.foreigners
+    @foreigner_year = foreigner_data.map{ |f| f.year}
     @embassies = @country.embassies
   end
 end
