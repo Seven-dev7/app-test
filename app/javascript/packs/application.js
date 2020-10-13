f16fdae66082862d6ad3cd20269bfc95237c2e1e
Se7en Oussama <seven.tdr7@gmail.com>
2020-10-13 10:22:49 +0200

    try to fix underscorejs pbm with heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require underscore
 //= require gmaps/google
 //= require_tree
